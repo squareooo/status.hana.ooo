f2b742dabea1866bc906ca397cf04d7ce5f31342
Hana <momenthana@kakao.com>
2021-08-15 18:42:40 +0900

    Change .eslintrc.js

--- src/App.vue
+++ src/App.vue
@@ -40,13 +40,9 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { defineComponent } from "@vue/composition-api"
 
-export default Vue.extend({
+export default defineComponent({
   name: "App",
-
-  data: () => ({
-    //
-  }),
-});
+})
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-alert type="success" />
+
+    <v-card-title>Current status</v-card-title>
+    <v-card></v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import Vue from "vue"
 
 export default Vue.extend({
   name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-});
+})
 </script>
