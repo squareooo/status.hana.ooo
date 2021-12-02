dd035544783cbb2afcc4f1d27834a37772baef66
Hana <momenthana@kakao.com>
2021-12-02 14:19:45 +0900

    Create remark gfm, math and highlight

--- src/views/Draft.vue
+++ src/views/Draft.vue
@@ -1,10 +1,14 @@
 <template>
   <v-layout fill-height>
     <v-sheet width="50%">
-      <v-textarea v-model="text" />
+      <v-container>
+        <v-textarea v-model="text" outlined full-width />
+      </v-container>
     </v-sheet>
     <v-sheet color="#7f00ff0f" width="50%">
-      <div v-html="html" />
+      <v-container>
+        <div v-html="html" />
+      </v-container>
     </v-sheet>
   </v-layout>
 </template>
@@ -18,7 +22,13 @@ import {
 } from "@vue/composition-api"
 import { unified } from "unified"
 import remarkParse from "remark-parse"
+import remarkBreaks from "remark-breaks"
 import remarkRehype from "remark-rehype"
+import remarkMath from "remark-math"
+import remarkGfm from "remark-gfm"
+import rehypeRaw from "rehype-raw"
+import rehypeKatex from "rehype-katex"
+import rehypeHighlight from "rehype-highlight"
 import rehypeStringify from "rehype-stringify"
 
 export default defineComponent({
@@ -32,7 +42,13 @@ export default defineComponent({
     const html = computed(() =>
       unified()
         .use(remarkParse)
-        .use(remarkRehype)
+        .use(remarkBreaks)
+        .use(remarkRehype, { allowDangerousHtml: true })
+        .use(remarkMath)
+        .use(remarkGfm)
+        .use(rehypeRaw)
+        .use(rehypeKatex)
+        .use(rehypeHighlight)
         .use(rehypeStringify)
         .processSync(state.text)
     )
