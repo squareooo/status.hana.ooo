1ab0b3f969ffc744d165a3f4dccaef5c4ffb6c7a
Hana <momenthana@kakao.com>
2022-04-16 08:34:41 +0900

    feat: createSeries mutation

---
+++ src/graphql/mutations/createSeries/resolver.ts
@@ -0,0 +1,23 @@
+import { AuthenticationError } from 'apollo-server-koa'
+import MUUID from 'uuid-mongodb'
+
+import Series from '@/models/series'
+
+export const createSeries = async (
+  _: any,
+  args: { input: any },
+  { payload }: any
+): Promise<any> => {
+  try {
+    if (payload == null) return new AuthenticationError('')
+
+    const series = new Series({
+      ...args.input,
+      userId: MUUID.from(payload.sub)
+    })
+
+    return await series.save()
+  } catch (err) {
+    console.error(err)
+  }
+}
