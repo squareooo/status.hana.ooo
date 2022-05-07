57096652462dd994f7b079da713ee5044f8a74d3
Hana <momenthana@kakao.com>
2022-05-08 04:37:19 +0900

    fix: deletedBlock resolver session

--- src/graphql/mutations/deleteBlock/resolver.ts
+++ src/graphql/mutations/deleteBlock/resolver.ts
@@ -1,12 +1,7 @@
 import { AuthenticationError } from 'apollo-server-core'
 import MUUID from 'uuid-mongodb'
-import { ListObjectsCommand, DeleteObjectsCommand } from '@aws-sdk/client-s3'
 
-import createConnection from '@/lib/createConnection'
-import { s3Client } from '@/lib/s3Client'
 import Block from '@/models/block'
-
-const test = createConnection('test')
 
 export const deleteBlock = async (
   _: any,
@@ -21,49 +16,15 @@ export const deleteBlock = async (
       '$1-$2-$3-$4-$5'
     )
 
-    const block = await Block.findOne({
-      id: MUUID.from(id),
-      userId: MUUID.from(payload.sub)
-    })
-    if (block == null) return
-
-    const session = await test.startSession()
-
-    let deletedBlock
-    await session.withTransaction(async () => {
-      const listObjectsCommand = new ListObjectsCommand({
-        Bucket: process.env.AWS_BUCKET,
-        Prefix: `${MUUID.from(id).toString('N')}/`
-      })
-      const listObjects = await s3Client.send(listObjectsCommand)
-
-      if (listObjects.Contents !== undefined) {
-        const deleteObjectsCommand = new DeleteObjectsCommand({
-          Bucket: process.env.AWS_BUCKET,
-          Delete: {
-            Objects: listObjects.Contents?.map((x) => ({
-              Key: x.Key
-            }))
-          }
-        })
-        await s3Client.send(deleteObjectsCommand)
+    const deletedBlock = await Block.findOneAndDelete(
+      {
+        id: MUUID.from(id),
+        userId: MUUID.from(payload.sub)
       }
+    )
+    if (deletedBlock == null) return
 
-      deletedBlock = await Block.findOneAndDelete(
-        {
-          id: MUUID.from(id),
-          userId: MUUID.from(payload.sub)
-        },
-        {
-          session
-        }
-      )
-      if (deletedBlock == null) return
-
-      deletedBlock.id = MUUID.from(deletedBlock.id).toString('N')
-    })
-
-    session.endSession().catch(() => {})
+    deletedBlock.id = MUUID.from(deletedBlock.id).toString('N')
 
     return deletedBlock
   } catch (err) {
