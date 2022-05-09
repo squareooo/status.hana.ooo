939ed9097c43f67882be6363067882c7eb7410dc
Hana <momenthana@kakao.com>
2022-05-09 21:32:27 +0900

    fix: docker localhost address input

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
-import { env } from 'process'
-import { ApolloServer } from 'apollo-server-fastify'
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
-import { ApolloServerPlugin } from 'apollo-server-plugin-base'
-import fastify, { FastifyInstance } from 'fastify'
-import Hana from 'hana.js'
+import { env } from "process";
+import { ApolloServer } from "apollo-server-fastify";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { ApolloServerPlugin } from "apollo-server-plugin-base";
+import fastify, { FastifyInstance } from "fastify";
+import Hana from "hana.js";
 
 const fastifyAppClosePlugin = (app: FastifyInstance): ApolloServerPlugin => {
   return {
-    async serverWillStart () {
+    async serverWillStart() {
       return {
-        async drainServer () {
-          await app.close()
-        }
-      }
-    }
-  }
-}
+        async drainServer() {
+          await app.close();
+        },
+      };
+    },
+  };
+};
 
 export const startApolloServer = async (
   typeDefs: any,
@@ -24,63 +24,63 @@ export const startApolloServer = async (
   const app = fastify({
     https: {
       key: env.KEY ?? undefined,
-      cert: env.CERT ?? undefined
-    }
-  })
+      cert: env.CERT ?? undefined,
+    },
+  });
   const server = new ApolloServer({
     context: async (ctx) => {
       try {
-        const { operationName }: any = ctx.request.body
+        const { operationName }: any = ctx.request.body;
 
-        if (operationName === 'IntrospectionQuery') {
-          if (env.NODE_ENV !== 'production') return { ctx }
+        if (operationName === "IntrospectionQuery") {
+          if (env.NODE_ENV !== "production") return { ctx };
 
-          if (ctx.request.headers.authorization == null) throw new Error()
+          if (ctx.request.headers.authorization == null) throw new Error();
 
-          const authorization = ctx.request.headers.authorization.split(' ')
+          const authorization = ctx.request.headers.authorization.split(" ");
 
-          if (authorization[1] !== env.TOKEN) throw new Error()
+          if (authorization[1] !== env.TOKEN) throw new Error();
         }
 
-        if (ctx.request.headers.authorization == null) return { ctx }
+        if (ctx.request.headers.authorization == null) return { ctx };
 
-        const authorization = ctx.request.headers.authorization.split(' ')
+        const authorization = ctx.request.headers.authorization.split(" ");
 
-        if (authorization[0] !== 'Bearer') return { ctx }
+        if (authorization[0] !== "Bearer") return { ctx };
 
         const payload = await Hana.Auth.tokenInfo({
-          accessToken: authorization[1]
-        })
+          accessToken: authorization[1],
+        });
 
-        return { ctx, payload }
+        return { ctx, payload };
       } catch {
-        return { ctx }
+        return { ctx };
       }
     },
     typeDefs,
     resolvers,
     plugins: [
       fastifyAppClosePlugin(app as any),
-      ApolloServerPluginDrainHttpServer({ httpServer: app.server as any })
-    ]
-  })
+      ApolloServerPluginDrainHttpServer({ httpServer: app.server as any }),
+    ],
+  });
 
-  await server.start()
-  app.register(import('@fastify/cookie'))
+  await server.start();
+  app.register(import("@fastify/cookie"));
   app.register(
     server.createHandler({
       cors: {
         origin: (_origin: any, cb: any) => {
-          cb(null, true)
+          cb(null, true);
         },
-        credentials: true
-      }
+        credentials: true,
+      },
     })
-  )
-  await app.listen(env.PORT ?? 4000)
+  );
+  await app.listen(env.PORT ?? 4000, env.ADDR);
   console.log(
     `🚀 Server ready at https://localhost:${env.PORT ?? 4000}${
       server.graphqlPath
     }`
-  )
-}
+  );
+};
